4965d4002ee3fa6a6bcc2c1035a68f53af43d9e3
Rizka404 <rizkamuhamad12@gmail.com>
2021-11-12 14:26:19 +0700

    PHP untuk tugas 12 Nov 2021 pemrogaman internet

---
+++ FormInput/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Membuat Form Sederhana</title>
+</head>
+<body>
+    <form>
+        <div>
+            <label>Masukan Nama Anda</label>
+            <input name="nama" type="text">
+            <button>Proses</button>
+        </div>
+    </form>
+	<?php
+			$nama = @$_GET['nama'];
+			?>
+	<?php
+			if ($nama) {
+			echo "Selamat Datang, {$nama} <br>";
+}
+?>
+</body>
+</html>
